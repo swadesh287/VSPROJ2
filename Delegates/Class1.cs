987de72723cc639cd4655548a6367becbfa12071
swadesh287 <40760833+swadesh287@users.noreply.github.com>
2018-07-02 19:47:05 +0530

    teste 2

--- Delegates/Class1.cs
+++ Delegates/Class1.cs
@@ -12,6 +12,7 @@ namespace Delegates
         public static void WriteToScreen(string str)
         {
             //Console.WriteLine("The String is: {0}", str);
+
             Console.WriteLine("Print");
 
         }
